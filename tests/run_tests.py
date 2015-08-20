f5e5352f927428df856457d07f13b60decbc7619
Thinesh Sornalingam <sornalingamt@woudc-px-dev1.cmc.ec.gc.ca>
2015-08-20 22:06:08 +0000

    init woudc-extcsv standalone lib

---
+++ tests/run_tests.py
@@ -0,0 +1,70 @@
+# =================================================================
+#
+# Terms and Conditions of Use
+#
+# Unless otherwise noted, computer program source code of this
+# distribution is covered under Crown Copyright, Government of
+# Canada, and is distributed under the MIT License.
+#
+# The Canada wordmark and related graphics associated with this
+# distribution are protected under trademark law and copyright law.
+# No permission is granted to use them outside the parameters of
+# the Government of Canada's corporate identity program. For
+# more information, see
+# http://www.tbs-sct.gc.ca/fip-pcim/index-eng.asp
+#
+# Copyright title to all 3rd party software distributed with this
+# software is held by the respective copyright holders as noted in
+# those files. Users are asked to read the 3rd Party Licenses
+# referenced with those assets.
+#
+# Copyright (c) YYYY Government of Canada
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# =================================================================
+
+import unittest
+
+
+def msg(test_id, test_description):
+    """convenience function to print out test id and desc"""
+    return '%s: %s' % (test_id, test_description)
+
+
+class FooTest(unittest.TestCase):
+    """Test suite for package Foo"""
+    def setUp(self):
+        """setup test fixtures, etc."""
+        print msg(self.id(), self.shortDescription())
+
+    def tearDown(self):
+        """return to pristine state"""
+        pass
+
+    def test_smoke(self):
+        """Simple Smoke Test"""
+        # test assertions go here
+        self.assertEquals(1, 1, 'Expected equality')
+
+if __name__ == '__main__':
+    unittest.main()
